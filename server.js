05eeb6167343b5f41ce22be6c66245f6beb35489
lallisindu <lallisindu123@gmail.com>
2023-12-15 00:07:06 +0530

    creating custom response

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome home');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to my Node Js project');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
